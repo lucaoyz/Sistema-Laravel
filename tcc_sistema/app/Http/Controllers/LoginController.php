7d1607156f6d324cacc468d131238a1fd4e403d8
lucaoyz <lucasfm.1406@gmail.com>
2022-03-01 21:48:18 -0300

    dashboard name and controller change

--- tcc_sistema/app/Http/Controllers/LoginController.php
+++ tcc_sistema/app/Http/Controllers/LoginController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class LoginController extends Controller
 {
     public function dashboard() {
-        return view('index');
+        return view('admin.dashboard');
     }
 }

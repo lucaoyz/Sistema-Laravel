3c7d93e60cf06e075fde61b24c17215107a73d3b
Lucas Moraes <lucasfm.1406@gmail.com>
2022-05-27 19:42:31 -0300

    tabela de parcelas da mensalidade

---
+++ tcc_sistema/database/migrations/2022_05_27_221104_create_parcelas_men_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('parcelas_men', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('men_id')->nullable()->constrained('mensalidade');
+            $table->foreignId('alu_id')->nullable()->constrained('alunos');
+            $table->date('par_data_pagto');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('parcelas_men');
+    }
+};

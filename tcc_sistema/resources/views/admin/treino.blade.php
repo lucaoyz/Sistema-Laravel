cee864ff57c4fd485f918e862af5b3c97812b101
Lucas Moraes <lucasfm.1406@gmail.com>
2022-06-16 23:30:20 -0300

    voltando um pouco atras

--- tcc_sistema/resources/views/admin/treino.blade.php
+++ tcc_sistema/resources/views/admin/treino.blade.php
@@ -44,21 +44,6 @@
           <div class="card-header p-3 pt-2">
             <div class="pt-1">
               <p class="text-sm mb-0 text-capitalize">Treinos</p>
-              <h5 class="mb-0"><a href="" data-bs-toggle="modal" data-bs-target="#TreinoModal" class="text-success">Tela de Treinos</a></h5>
-            </div>
-          </div>
-          <hr class="dark horizontal my-0">
-          <div class="card-footer p-3">
-            <p class="mb-0"><span class="text-sm font-weight-bolder">aaaaaaaa
-                </span></p>
-          </div>
-        </div>
-      </div>
-      <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
-        <div class="card">
-          <div class="card-header p-3 pt-2">
-            <div class="pt-1">
-              <p class="text-sm mb-0 text-capitalize">Treinos</p>
               <h5 class="mb-0"><a href="" class="text-success">Crie o treino para um aluno</a></h5>
             </div>
           </div>

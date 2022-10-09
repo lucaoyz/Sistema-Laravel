219fab2b0185e1f28beed92f6fc0cbbc0f73bdca
Lucas Moraes <lucasfm.1406@gmail.com>
2022-10-09 11:58:03 -0300

    Layouts modificados

--- resources/views/aluno/layoutsModals/layouts.blade.php
+++ resources/views/aluno/layoutsModals/layouts.blade.php
@@ -206,31 +206,6 @@
           </div>
           <hr class="horizontal dark my-1">
           <div class="card-body pt-sm-3 pt-0">
-            <!-- Sidebar Backgrounds -->
-            <div>
-              <h6 class="mb-0">Cor da barra lateral</h6>
-            </div>
-            <a href="javascript:void(0)" class="switch-trigger background-color">
-              <div class="badge-colors my-2 text-start">
-                <span class="badge filter bg-gradient-primary active" data-color="primary" onclick="sidebarColor(this)"></span>
-                <span class="badge filter bg-gradient-dark" data-color="dark" onclick="sidebarColor(this)"></span>
-                <span class="badge filter bg-gradient-info" data-color="info" onclick="sidebarColor(this)"></span>
-                <span class="badge filter bg-gradient-success" data-color="success" onclick="sidebarColor(this)"></span>
-                <span class="badge filter bg-gradient-warning" data-color="warning" onclick="sidebarColor(this)"></span>
-                <span class="badge filter bg-gradient-danger" data-color="danger" onclick="sidebarColor(this)"></span>
-              </div>
-            </a>
-            <!-- Sidenav Type -->
-            <div class="mt-3">
-              <h6 class="mb-0">Tipo de barra lateral</h6>
-              <p class="text-sm">Selecione o tipo de sua barra lateral.</p>
-            </div>
-            <div class="d-flex">
-              <button class="btn bg-gradient-dark px-3 mb-2 active" data-class="bg-gradient-dark" onclick="sidebarType(this)">Dark</button>
-              <button class="btn bg-gradient-dark px-3 mb-2 ms-2" data-class="bg-transparent" onclick="sidebarType(this)">Transparent</button>
-              <button class="btn bg-gradient-dark px-3 mb-2 ms-2" data-class="bg-white" onclick="sidebarType(this)">White</button>
-            </div>
-            <p class="text-sm d-xl-none d-block mt-2">Você só pode alterar o tipo de barra lateral na versão de computador.</p>
             <!-- Dark Mode -->
 
             <hr class="horizontal dark my-3">
@@ -240,6 +215,10 @@
                 <input class="form-check-input mt-1 ms-auto" type="checkbox" id="dark-version" onclick="darkMode(this)">
               </div>
             </div>
+            <!-- Aviso -->
+            <div class="mt-3">
+            <h6 class="mb-0">Mais alterações visuais em desenvolvimento.</h6>
+
             <hr class="horizontal dark my-sm-4">
             <div class="w-100 text-center">
               <a class="github-button" href="https://github.com/lucaoyz/sistema-laravel" data-icon="octicon-star" data-size="large" data-show-count="true" aria-label="Star creativetimofficial/material-dashboard on GitHub">Star</a>

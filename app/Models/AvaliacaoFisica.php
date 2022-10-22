d294683fff84b3f61ae627ea53d7fc2f83bf6609
Lucas Moraes <lucasfm.1406@gmail.com>
2022-10-22 15:55:59 -0300

    migration e model avaliacao fisica

---
+++ app/Models/AvaliacaoFisica.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AvaliacaoFisica extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'per_id',
+        'alu_id',
+        'af_kg',
+        'af_massa_gorda',
+        'af_massa_magra',
+        'af_imc',
+        'af_cm_bracoE',
+        'af_cm_bracoD',
+        'af_cm_antebracoE',
+        'af_cm_antebracoD',
+        'af_cm_coxaE',
+        'af_cm_coxaD',
+        'af_cm_panturrilhaE',
+        'af_cm_panturrilhaD',
+        'af_cm_torax',
+        'af_cm_cintura',
+        'af_cm_abdomen',
+        'af_cm_quadril',
+        'af_cm_pescoco',
+        'af_cm_ombro',
+        'af_dc_subescapular',
+        'af_dc_triceps',
+        'af_dc_biceps',
+        'af_dc_torax',
+        'af_dc_axilarMedia',
+        'af_dc_suprailiaca',
+        'af_dc_abdominal',
+        'af_dc_coxaMedial',
+        'af_dc_panturrilhaMedial',
+        'af_objetivo',
+    ];
+
+    public function alunoId()
+    {
+        return $this->belongsTo(Aluno::class, 'alu_id', 'id');
+    }
+
+    public function personalId()
+    {
+        return $this->belongsTo(Personal::class, 'per_id', 'id');
+    }
+}
---
+++ database/migrations/2022_10_22_184418_create_avaliacao_fisicas_table.php
@@ -0,0 +1,61 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('avaliacao_fisicas', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('per_id')->nullable()->constrained('personals');
+            $table->foreignId('alu_id')->nullable()->constrained('alunos');
+            $table->string('af_kg')->nullable();
+            $table->string('af_massa_gorda')->nullable();
+            $table->string('af_massa_magra')->nullable();
+            $table->string('af_imc')->nullable();
+            $table->string('af_cm_bracoE')->nullable();
+            $table->string('af_cm_bracoD')->nullable();
+            $table->string('af_cm_antebracoE')->nullable();
+            $table->string('af_cm_antebracoD')->nullable();
+            $table->string('af_cm_coxaE')->nullable();
+            $table->string('af_cm_coxaD')->nullable();
+            $table->string('af_cm_panturrilhaE')->nullable();
+            $table->string('af_cm_panturrilhaD')->nullable();
+            $table->string('af_cm_torax')->nullable();
+            $table->string('af_cm_cintura')->nullable();
+            $table->string('af_cm_abdomen')->nullable();
+            $table->string('af_cm_quadril')->nullable();
+            $table->string('af_cm_pescoco')->nullable();
+            $table->string('af_cm_ombro')->nullable();
+            $table->string('af_dc_subescapular')->nullable();
+            $table->string('af_dc_triceps')->nullable();
+            $table->string('af_dc_biceps')->nullable();
+            $table->string('af_dc_torax')->nullable();
+            $table->string('af_dc_axilarMedia')->nullable();
+            $table->string('af_dc_suprailiaca')->nullable();
+            $table->string('af_dc_abdominal')->nullable();
+            $table->string('af_dc_coxaMedial')->nullable();
+            $table->string('af_dc_panturrilhaMedial')->nullable();
+            $table->string('af_objetivo')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('avaliacao_fisicas');
+    }
+};

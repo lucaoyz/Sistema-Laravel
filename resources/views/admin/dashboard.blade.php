c64713349083249c3696f2254543c16009247f99
lucaoyz <lucasfm.1406@gmail.com>
2022-10-24 20:27:14 -0300

    Botão de suporte

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -81,6 +81,27 @@
             </div>
           </div>
         </div>
+      </div><br>
+      <div class="row">
+        <div class="col-xl-3 col-sm-6">
+          <div class="card">
+            <div class="card-header p-3 pt-2">
+                <a href="https://api.whatsapp.com/send?phone=5519991415811&text=Ol%C3%A1%2C%20vim%20pelo%20sistema%20e%20preciso%20de%20uma%20ajuda!">
+              <div class="icon icon-lg icon-shape bg-gradient-secondary shadow-secondary text-center border-radius-xl mt-n4 position-absolute">
+                <i class="material-icons opacity-10">settings_accessibility</i>
+              </div>
+            </a>
+              <div class="text-end pt-1">
+                <p class="text-sm mb-0 text-capitalize">Suporte</p>
+                <h4 class="mb-0"><a href="https://api.whatsapp.com/send?phone=5519991415811&text=Ol%C3%A1%2C%20vim%20pelo%20sistema%20e%20preciso%20de%20uma%20ajuda!">Fale conosco</a></h4>
+              </div>
+            </div>
+            <hr class="dark horizontal my-0">
+            <div class="card-footer p-3">
+                <p class="mb-0"><span class="text-danger text-sm font-weight-bolder"><a href="https://api.whatsapp.com/send?phone=5519991415811&text=Ol%C3%A1%2C%20vim%20pelo%20sistema%20e%20preciso%20de%20uma%20ajuda!">Clique aqui caso para alguma dúvida!</a></span></p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
       @endsection
